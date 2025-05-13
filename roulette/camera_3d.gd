dd7c213dc15b0f32505bccbc27af67da05dcc5db
LilMasterOogway <110360959+LilMasterOogway@users.noreply.github.com>
2025-05-14 09:57:23 +1200

    Started Volume Slider

--- roulette/camera_3d.gd
+++ roulette/camera_3d.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var mouse_sensitivity: float = 0.005
 @onready var v_box_container: VBoxContainer = $"../VBoxContainer"
 @onready var v_box_container_2: VBoxContainer = $"../VBoxContainer2"
+@onready var v_box_container_3: VBoxContainer = $"../VBoxContainer3"
 
 # Angle limits (in degrees)
 @export var max_pitch: float = 80.0  # Up/down limit
@@ -14,10 +15,26 @@ var game_start = false
 # Current rotation
 var pitch: float = 0.0
 var yaw: float = 0.0
+@onready var volume_slider = $"../VBoxContainer3/HSlider"
 
 func _ready():
-	pass
-	# Capture mouse
+	v_box_container_3.hide()
+	# Set initial value (0 to 1 range for volume)
+	volume_slider.min_value = 0.0
+	volume_slider.max_value = 1.0
+	volume_slider.value = 0.8  # Default volume at 80%
+	
+	# Connect the value_changed signal
+	volume_slider.connect("value_changed", _on_volume_changed)
+	
+	# Set initial volume (correct way)
+	_on_volume_changed(volume_slider.value)
+
+# This function will be called when the slider changes
+func _on_volume_changed(value):
+	# Convert to decibels and set the bus volume
+	var db = linear_to_db(value)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), db)
 	
 func _input(event):
 	if event is InputEventMouseMotion and game_start:
@@ -51,4 +68,19 @@ func _on_button_pressed() -> void:
 	v_box_container_2.hide()
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	game_start = true
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
+	v_box_container_2.hide()
+	v_box_container_3.show()
+	pass # Replace with function body.
+
+
+func _on_button_3_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_h_slider_drag_ended(value_changed: bool) -> void:
 	pass # Replace with function body.
